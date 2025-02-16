1218fe150e556841cb843dfa900be84a9e829eda
Danylo Parii <danylo.parii@nordcurrent.com>
2025-02-16 22:48:15 +0100

    add physics. add config and parsing.

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -1,12 +1,19 @@
 #include "GameScene.h"
-#include "SimpleAudioEngine.h"
+#include "json/document.h"
+#include "json/filereadstream.h"
+#include "json/reader.h"
 
 USING_NS_CC;
 
+const int spaceshipBitMask = 0x01;
+const int asteroidBitMask = 0x02;
+const int bulletBitMask = 0x04;
+const int boundsBitmask = 0x08;
+
 GameScene::GameScene()
     : mSpaceship(nullptr)
-    , mBulletCooldown(0.3f)
     , mIsCanShoot(true)
+    , mIsMousePressed(false)
 {
 
 }
@@ -18,77 +25,183 @@ GameScene::~GameScene()
 
 Scene* GameScene::createScene()
 {
-    auto scene = GameScene::create();
-    if (!scene)
+    Scene* ret = new (std::nothrow) GameScene();
+    if (ret && ret->initWithPhysics() && ret->init())
+    {
+        ret->autorelease();
+        return ret;
+    }
+    else
+    {
+        CC_SAFE_DELETE(ret);
         return nullptr;
-    return scene;
+    }
+}
+
+void GameScene::parseConfig()
+{
+    std::string fileContent = FileUtils::getInstance()->getStringFromFile("config.json");
+
+    rapidjson::Document document;
+    document.Parse(fileContent.c_str());
+
+    if (document.HasParseError())
+        return;
+
+    auto parseFloat = [](const rapidjson::Value& aSource, const char* aKey, float& aDestination)
+    {
+        if (aSource.HasMember(aKey) && aSource[aKey].IsFloat())
+        {
+            aDestination = aSource[aKey].GetFloat();
+        }
+    };
+
+    if (document.HasMember("spaceship") && document["spaceship"].IsObject())
+    {
+        const auto& spaceshipJson = document["spaceship"];
+        parseFloat(spaceshipJson, "linearDamping", mSpaceshipConfig.linearDamping);
+        parseFloat(spaceshipJson, "mass", mSpaceshipConfig.mass);
+        parseFloat(spaceshipJson, "acceleration", mSpaceshipConfig.acceleration);
+        parseFloat(spaceshipJson, "bulletCooldown", mSpaceshipConfig.bulletCooldown);
+    }
+
+    if (document.HasMember("bullet") && document["bullet"].IsObject())
+    {
+        const auto& bulletJson = document["bullet"];
+        parseFloat(bulletJson, "acceleration", mBulletConfig.acceleration);
+    }
 }
 
 bool GameScene::init()
 {
-    if (!Scene::init())
-        return false;
-
-    const auto visibleSize = Director::getInstance()->getVisibleSize();
-    const Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
-    auto background = Sprite::create("background.jpg");
-    if (background)
+    if (_physicsWorld)
     {
-        background->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y);
-        this->addChild(background, 0);
+        _physicsWorld->setGravity(Vec2(0, 0));
+        _physicsWorld->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
     }
 
-    mSpaceship = Sprite::create("spaceship.png");
-    if (mSpaceship)
-    {
-        mSpaceship->setPosition(visibleSize.width / 2, visibleSize.height / 2);
-        this->addChild(mSpaceship, 1);
-    }
+    parseConfig();
+    createScreenBounds();
+    createBackground();
+    createSpaceship();
 
     auto keyboardListener = EventListenerKeyboard::create();
     keyboardListener->onKeyPressed = CC_CALLBACK_2(GameScene::onKeyPressed, this);
     keyboardListener->onKeyReleased = CC_CALLBACK_2(GameScene::onKeyReleased, this);
     _eventDispatcher->addEventListenerWithSceneGraphPriority(keyboardListener, this);
-    
+
     auto listener = EventListenerMouse::create();
     listener->onMouseMove = CC_CALLBACK_1(GameScene::onMouseMove, this);
     listener->onMouseDown = CC_CALLBACK_1(GameScene::onMouseDown, this);
+    listener->onMouseUp = CC_CALLBACK_1(GameScene::onMouseUp, this);
     _eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
 
+    auto contactListener = EventListenerPhysicsContact::create();
+    contactListener->onContactBegin = CC_CALLBACK_1(GameScene::onContactBegin, this);
+    contactListener->onContactSeparate = CC_CALLBACK_1(GameScene::onContactSeparate, this);
+    _eventDispatcher->addEventListenerWithSceneGraphPriority(contactListener, this);
+
+    schedule(CC_SCHEDULE_SELECTOR(GameScene::spawnAsteroid), 1.5f);
 
     this->scheduleUpdate();
 
     return true;
 }
 
+void GameScene::createScreenBounds()
+{
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    auto origin = Director::getInstance()->getVisibleOrigin();
+
+    auto edgeNode = Node::create();
+    if (edgeNode)
+    {
+        edgeNode->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y);
+        auto edgeBody = PhysicsBody::createEdgeBox(
+            visibleSize,
+            PhysicsMaterial(1.0f, 0.0f, 0.0f),
+            3
+        );
+        if (edgeBody)
+        {
+            edgeBody->setDynamic(false);
+            edgeBody->setCategoryBitmask(boundsBitmask);
+            edgeBody->setCollisionBitmask(spaceshipBitMask);
+            edgeBody->setContactTestBitmask(bulletBitMask);
+            edgeNode->setPhysicsBody(edgeBody);
+        }
+    }
+
+    this->addChild(edgeNode);
+}
+
+void GameScene::createBackground()
+{
+    const auto visibleSize = Director::getInstance()->getVisibleSize();
+    const Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    auto background = Sprite::create("background.jpg");
+    if (background)
+    {
+        background->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y);
+        this->addChild(background, 0);
+    }
+}
+
+void GameScene::createSpaceship()
+{
+    const auto visibleSize = Director::getInstance()->getVisibleSize();
+    mSpaceship = Sprite::create("spaceship.png");
+    if (mSpaceship)
+    {
+        mSpaceship->setPosition(visibleSize.width / 2, visibleSize.height / 2);
+        this->addChild(mSpaceship, 1);
+
+        auto spaceshipBody = PhysicsBody::createCircle(mSpaceship->getContentSize().width / 2);
+        if (spaceshipBody)
+        {
+            spaceshipBody->setCategoryBitmask(spaceshipBitMask);
+            spaceshipBody->setCollisionBitmask(asteroidBitMask | boundsBitmask);
+            spaceshipBody->setContactTestBitmask(asteroidBitMask);
+            spaceshipBody->setDynamic(true);
+            spaceshipBody->setRotationEnable(false);
+            spaceshipBody->setLinearDamping(mSpaceshipConfig.linearDamping);
+            spaceshipBody->setMass(mSpaceshipConfig.mass);
+            mSpaceship->setPhysicsBody(spaceshipBody);
+        }
+    }
+}
+
+void GameScene::spawnAsteroid(float dt)
+{
+}
+
 void GameScene::update(float aDelta)
 {
     if (mSpaceship)
     {
-        const int speed = 200;
-        const auto visibleSize = Director::getInstance()->getVisibleSize();
+        auto body = mSpaceship->getPhysicsBody();
+        Vec2 force;
 
-        Vec2 position = mSpaceship->getPosition();
-        auto bbox = mSpaceship->getBoundingBox();
+        if (mPressedKeys.count(EventKeyboard::KeyCode::KEY_W) || mPressedKeys.count(EventKeyboard::KeyCode::KEY_UP_ARROW))
+        {
+            force += Vec2(0, mSpaceshipConfig.acceleration);
+        }
+        if (mPressedKeys.count(EventKeyboard::KeyCode::KEY_S) || mPressedKeys.count(EventKeyboard::KeyCode::KEY_DOWN_ARROW))
+        {
+            force += Vec2(0, -mSpaceshipConfig.acceleration);
+        }
+        if (mPressedKeys.count(EventKeyboard::KeyCode::KEY_A) || mPressedKeys.count(EventKeyboard::KeyCode::KEY_LEFT_ARROW))
+        {
+            force += Vec2(-mSpaceshipConfig.acceleration, 0);
+        }
+        if (mPressedKeys.count(EventKeyboard::KeyCode::KEY_D) || mPressedKeys.count(EventKeyboard::KeyCode::KEY_RIGHT_ARROW))
+        {
+            force += Vec2(mSpaceshipConfig.acceleration, 0);
+        }
 
-        if (position.y + speed * aDelta <= visibleSize.height - bbox.size.height / 2)
-            if (mPressedKeys.count(EventKeyboard::KeyCode::KEY_W) || mPressedKeys.count(EventKeyboard::KeyCode::KEY_UP_ARROW))
-                position.y += speed * aDelta;
-        if (position.y - speed * aDelta >= bbox.size.height / 2)
-            if (mPressedKeys.count(EventKeyboard::KeyCode::KEY_S) || mPressedKeys.count(EventKeyboard::KeyCode::KEY_DOWN_ARROW))
-                position.y -= speed * aDelta;
-        if (position.x - speed * aDelta >= bbox.size.width / 2)
-            if (mPressedKeys.count(EventKeyboard::KeyCode::KEY_A) || mPressedKeys.count(EventKeyboard::KeyCode::KEY_LEFT_ARROW))
-                position.x -= speed * aDelta;
-        if (position.x + speed * aDelta <= visibleSize.width - bbox.size.width / 2)
-            if (mPressedKeys.count(EventKeyboard::KeyCode::KEY_D) || mPressedKeys.count(EventKeyboard::KeyCode::KEY_RIGHT_ARROW))
-                position.x += speed * aDelta;
-
-        mSpaceship->setPosition(position);
-
-        adjustSpaceshipRotation();
+        body->applyForce(force);
     }
+    adjustSpaceshipRotation();
 }
 
 void GameScene::onKeyPressed(EventKeyboard::KeyCode aKeyCode, Event* aEvent)
@@ -112,40 +225,102 @@ void GameScene::onMouseMove(EventMouse* aEvent)
 
 void GameScene::adjustSpaceshipRotation()
 {
-    Vec2 direction = mMousePosition - mSpaceship->getPosition();
-    float angle = CC_RADIANS_TO_DEGREES(atan2(direction.y, direction.x));
-    // due to spaceship head looking up, we have to subtract 90 degrees
-    angle -= 90;
-    mSpaceship->setRotation(-angle);
+    if (mSpaceship)
+    {
+        Vec2 direction = mMousePosition - mSpaceship->getPosition();
+        float angle = CC_RADIANS_TO_DEGREES(atan2(direction.y, direction.x));
+        // due to spaceship head looking up, we have to subtract 90 degrees
+        angle -= 90;
+        mSpaceship->setRotation(-angle);
+    }
 }
 
 void GameScene::onMouseDown(EventMouse* aEvent)
 {
-    if (aEvent && aEvent->getMouseButton() == EventMouse::MouseButton::BUTTON_LEFT && mIsCanShoot)
+    if (aEvent && aEvent->getMouseButton() == EventMouse::MouseButton::BUTTON_LEFT)
     {
-        mIsCanShoot = false;
-        shootBullet(aEvent->getLocationInView());
-        this->scheduleOnce([this](float) { mIsCanShoot = true; }, mBulletCooldown, "fireCooldown");
+        mIsMousePressed = true;
+        if (mIsCanShoot)
+        {
+            shootBullet(aEvent->getLocationInView());
+        }
     }
 }
 
-void GameScene::shootBullet(Vec2 target)
+void GameScene::onMouseUp(cocos2d::EventMouse* aEvent)
+{
+    mIsMousePressed = false;
+}
+
+bool GameScene::onContactBegin(cocos2d::PhysicsContact& contact)
+{
+    return false;
+}
+
+bool GameScene::onContactSeparate(cocos2d::PhysicsContact& contact)
+{
+    auto shapeA = contact.getShapeA();
+    auto shapeB = contact.getShapeB();
+
+    auto bodyA = shapeA->getBody();
+    auto bodyB = shapeB->getBody();
+
+    if (!bodyA || !bodyB)
+        return false;
+
+    if ((bodyA->getCategoryBitmask() == bulletBitMask && bodyB->getCategoryBitmask() == boundsBitmask)
+        || (bodyA->getCategoryBitmask() == boundsBitmask && bodyB->getCategoryBitmask() == bulletBitMask))
+    {
+        if (bodyA->getCategoryBitmask() == bulletBitMask)
+        {
+            if (bodyA->getNode())
+                bodyA->getNode()->removeFromParent();
+        }
+        if (bodyB->getCategoryBitmask() == bulletBitMask)
+        {
+            if (bodyB->getNode())
+                bodyB->getNode()->removeFromParent();
+        }
+    }
+
+    return true;
+}
+
+void GameScene::shootBullet(Vec2 aTarget)
 {
     if (mSpaceship)
     {
         auto bullet = Sprite::create("missile.png");
-        bullet->setPosition(mSpaceship->getPosition());
-        bullet->setRotation(mSpaceship->getRotation());
-        this->addChild(bullet);
+        if (bullet)
+        {
+            mIsCanShoot = false;
 
-        Vec2 direction = target - bullet->getPosition();
-        direction.normalize();
+            bullet->setPosition(mSpaceship->getPosition());
+            this->addChild(bullet);
 
-        float speed = 500.0f;
-        Vec2 velocity = direction * speed;
+            Vec2 direction = aTarget - bullet->getPosition();
+            direction.normalize();
+            Vec2 velocity = direction * mBulletConfig.acceleration;
 
-        auto moveAction = MoveBy::create(1.0f, velocity);
-        auto removeAction = RemoveSelf::create();
-        bullet->runAction(Sequence::create(moveAction, removeAction, nullptr));
+            auto bulletBody = PhysicsBody::createBox(bullet->getContentSize());
+            if (bulletBody)
+            {
+                bulletBody->setCategoryBitmask(bulletBitMask);
+                bulletBody->setContactTestBitmask(boundsBitmask);
+                bulletBody->setVelocity(velocity);
+                bulletBody->setGravityEnable(false);
+                bulletBody->setRotationEnable(false);
+                bullet->setPhysicsBody(bulletBody);
+            }
+            bullet->setRotation(mSpaceship->getRotation());
+
+            this->scheduleOnce([this](float)
+                {
+                    mIsCanShoot = true;
+                    if (mIsMousePressed)
+                        shootBullet(mMousePosition);
+                }, mSpaceshipConfig.bulletCooldown, "fireCooldown"
+            );
+        }
     }
 }
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -8,31 +8,66 @@ class GameScene
     : public cocos2d::Scene
 {
 private:
+    struct sSpaceshipConfig
+    {
+        float linearDamping;
+        float mass;
+        float acceleration;
+        float bulletCooldown;
+
+        sSpaceshipConfig()
+            : linearDamping(1.f)
+            , mass(1.f)
+            , acceleration(500.f)
+            , bulletCooldown(0.3f)
+        {
+        }
+    };
+
+    struct sBulletConfig
+    {
+        float acceleration;
+
+        sBulletConfig()
+            : acceleration(500.f)
+        {
+        }
+    };
+
+private:
     cocos2d::Sprite* mSpaceship;
     std::unordered_set<cocos2d::EventKeyboard::KeyCode> mPressedKeys;
     cocos2d::Vec2 mMousePosition;
-    float mBulletCooldown;
+    sSpaceshipConfig mSpaceshipConfig;
+    sBulletConfig mBulletConfig;
+    bool mIsMousePressed;
     bool mIsCanShoot;
 
 private:
     bool init() override;
+    void parseConfig();
+    void createBackground();
+    void createSpaceship();
+    void createScreenBounds();
     void update(float aDelta) override;
 
     void onKeyPressed(cocos2d::EventKeyboard::KeyCode aKeyCode, cocos2d::Event* aEvent);
     void onKeyReleased(cocos2d::EventKeyboard::KeyCode aKeyCode, cocos2d::Event* aEvent);
     void onMouseMove(cocos2d::EventMouse* aEvent);
     void onMouseDown(cocos2d::EventMouse* aEvent);
+    void onMouseUp(cocos2d::EventMouse* aEvent);
+    bool onContactBegin(cocos2d::PhysicsContact& contact);
+    bool onContactSeparate(cocos2d::PhysicsContact& contact);
 
     void shootBullet(cocos2d::Vec2 target);
     void adjustSpaceshipRotation();
+    void spawnAsteroid(float dt);
 
 public:
     GameScene();
     virtual ~GameScene();
 
     static cocos2d::Scene* createScene();
-
-    CREATE_FUNC(GameScene);
 };
 
 #endif // __GAME_SCENE_H__
